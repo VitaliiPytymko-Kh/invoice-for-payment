5e4301dc058801bf313c20dabad4cb7f16d2fa90
VitaliiPytymko <vitaliipytymko@gmail.com>
2024-01-07 21:35:18 +0200

    добавить реализацию исключения !!!! по полю "штраф"

--- invoice for payment/IFP.cs
+++ invoice for payment/IFP.cs
@@ -1,23 +1,72 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace invoice_for_payment
 {
+
+    [Serializable]
+    [DataContract]
     public class IFP
     {
+        [DataMember]   
         public int PayForDay { get; set; }//ОплатаЗаДень 
+        [DataMember]
         public int AmountOfDAys { get; set; }//КоличествоДней 
+        [DataMember]
         public int Payford { get; set; }//Штраф За Один День Задержки Оплаты 
+        [DataMember]
         public int AmountDayPD { get; set; }//КоличествоДнейЗадержкиОплаты 
 
-        public decimal AmountCOpaymentWithout_Penalty { get { return PayForDay * AmountOfDAys; } }//СуммаКОплатеБезШтрафа 
-        public decimal Forfeit { get { return AmountDayPD * PayForDay; } }//Штраф 
-        public decimal TotalAmountCOPayment { get{ return AmountCOpaymentWithout_Penalty + Forfeit;  } }//ОбщаяСуммаКОплате 
+        [DataMember]
+        public decimal AmountCOpaymentWithout_Penalty //СуммаКОплатеБезШтрафа 
+        {
+            get { return PayForDay * AmountOfDAys; }
+            set { }
+        }
+        [DataMember]
+        public decimal Forfeit//Штраф /// добавить реализацию исключения !!!!
+        { get { return AmountDayPD * PayForDay; }
+            set { }
+        } 
 
-        public static bool Serialize_Computed_Fields { get; set; }= true;
+        [DataMember]
+        public decimal TotalAmountCOPayment //ОбщаяСуммаКОплате 
+        { get{ return AmountCOpaymentWithout_Penalty + Forfeit;  }
+            set { }
+        }
+        
+
+        public IFP() { }
+        public IFP (int payForDay, int amountOfDAys, int payford, int amountDayPD)
+        {
+            PayForDay = payForDay;
+            AmountOfDAys = amountOfDAys;
+            Payford = payford;
+            AmountDayPD = amountDayPD;
+        }
+    
+
+        public static bool SerializeCalculated { get; set; }=true;
+
+        public override string ToString()
+        {
+            return $"ОплатаЗаДень:{PayForDay}\n" +
+                $" КоличествоДней :{AmountOfDAys}\n" +
+                $" Штраф За Один День Задержки Оплаты :{Payford}\n"+
+                $"КоличествоДнейЗадержкиОплаты : {AmountDayPD}\n"+
+                $"СуммаКОплатеБезШтрафа:{AmountCOpaymentWithout_Penalty}\n"+
+                  $"Штраф:{Forfeit}\n" +
+                    $"ОбщаяСуммаКОплате:{TotalAmountCOPayment}\n";
+        }
+
 
     }
+
 }
--- invoice for payment/Program.cs
+++ invoice for payment/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace invoice_for_payment
 {
@@ -10,6 +13,11 @@ namespace invoice_for_payment
     {
         static void Main(string[] args)
         {
+            IFP iFP = new IFP(150,20,15,30);
+            FileStream fileStream = new FileStream("IFP3.json",FileMode.Create);
+            DataContractJsonSerializer data=new DataContractJsonSerializer(typeof(IFP));
+            data.WriteObject(fileStream, iFP);
+            fileStream.Close();
         }
     }
 }
